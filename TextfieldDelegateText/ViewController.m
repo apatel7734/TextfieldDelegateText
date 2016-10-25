1fdd3d76e32bbe0685ccdcf09e2f28f7703972a9
Ashish Patel <a.patel@capitalone.com>
2016-10-25 13:40:33 -0700

    add 6,7,8

--- TextfieldDelegateText/ViewController.m
+++ TextfieldDelegateText/ViewController.m
@@ -26,6 +26,9 @@
     //Test commit 3
     //Test commit 4
     //Test commit 5
+    //Test commit 6
+    //Test commit 7
+    //Test commit 8
 }
 
 -(BOOL) textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
